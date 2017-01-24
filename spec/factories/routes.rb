e9abfc57edf6e27bdd3336e6a781350a18114a21
Aleksey Grischenko <alek.grischenko@gmail.com>
2017-01-24 14:54:36 +0300

    Add tests for API

---
+++ spec/factories/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'faker'
+
+FactoryGirl.define do
+  factory :route do
+    source Route::SOURCES.sample
+    start_node Route::NODES.sample
+    end_node Route::NODES.sample
+    start_date DateTime.now
+    end_date { DateTime.now + 1.hour }
+  end
+end
